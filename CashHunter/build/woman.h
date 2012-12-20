f6524d1202bc36475b4a2308f7f973e731e8bb1b
TaeUng <yukult400@naver.com>
2012-12-20 14:17:50 +0900

    Finish Sound 교체

--- CashHunter/build/woman.h
+++ CashHunter/build/woman.h
@@ -8,7 +8,7 @@
 //	+ 192 tiles Metatiled by 4x4 not compressed
 //	Total size: 512 + 12288 = 12800
 //
-//	Time-stamp: 2012-12-20, 14:09:39
+//	Time-stamp: 2012-12-20, 14:17:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.9
 //	( http://www.coranac.com/projects/#grit )
 //
